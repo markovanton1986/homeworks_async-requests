e4a870afdfc985e0a2b30130a21748564729c9ed
Антон <110550719+markovanton1986@users.noreply.github.com>
2024-04-11 16:11:01 +0600

    Add files via upload

--- progressbar/task.js
+++ progressbar/task.js
@@ -1,31 +1,20 @@
-const progress = document.getElementById( 'progress' );
-
+const progress = document.getElementById('progress');
+progress.value = 0.7;
 const form = document.getElementById('form');
 
-form.addEventListener('submit', (e) => {
-
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
     const formData = new FormData(form);
-    runXHR(formData);
-
-    e.preventDefault();
-});
-
-function fn(e) {
-    progress.value = 0.7;
-};
-
-function addListeners(xhr) {
-    xhr.addEventListener('loadstart', fn);
-    xhr.addEventListener('load', fn);
-    xhr.addEventListener('loadend', fn);
-    xhr.addEventListener('progress', fn);
-    xhr.addEventListener('error', fn);
-};
-
-function runXHR(formData) {
     const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
+    xhr.onload = () => {
+        console.log(xhr.response);
+    }
+    
 
-    xhr.open("POST", 'https://students.netoservices.ru/nestjs-backend/upload');
-    addListeners(xhr)
+    xhr.upload.addEventListener('progress', (event) => {
+        console.log(event);
+        progress.value = event.loaded / event.total;
+    })
     xhr.send(formData);
-};
+})
